bab2213933d62f1c83291ce30dacb193692516cb
Anurag Rana <anuragrana31189@gmail.com>
2018-03-12 16:01:45 +0530

    added script to get system information

---
+++ system_information.py
@@ -0,0 +1,54 @@
+#
+# Python script to fetch system information
+# Author -  ThePythonDjango.Com
+# Tested with Python3 on Ubuntu 16.04
+# 
+
+import platform
+
+# Architecture
+print("Architecture: " + platform.architecture()[0])
+
+# machine
+print("Machine: " + platform.machine())
+
+# node
+print("Node: " + platform.node())
+
+# processor
+print("Processors: ")
+with open("/proc/cpuinfo", "r")  as f:
+    info = f.readlines()
+
+cpuinfo = [x.strip().split(":")[1] for x in info if "model name"  in x]
+for index, item in enumerate(cpuinfo):
+    print(" " + str(index) + ": " + item)
+
+# system
+print("System: " + platform.system())
+
+# distribution
+dist = platform.dist()
+dist = " ".join(x for x in dist)
+print("Distribution: " + dist)
+
+# Load
+with open("/proc/loadavg", "r") as f:
+    print("Average Load: " + f.read().strip())
+
+# Memory
+print("Memory Info: ")
+with open("/proc/meminfo", "r") as f:
+    lines = f.readlines()
+
+print(" " + lines[0].strip())
+print(" " + lines[1].strip())
+
+# uptime
+uptime = None
+with open("/proc/uptime", "r") as f:
+    uptime = f.read().split(" ")[0].strip()
+uptime = int(float(uptime))
+uptime_hours = uptime // 3600
+uptime_minutes = (uptime % 3600) // 60
+print("Uptime: " + str(uptime_hours) + ":" + str(uptime_minutes) + " hours")
