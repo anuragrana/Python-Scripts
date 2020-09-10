6b1235ae5f44adea3b5c49f829b7b59de22afdf5
rana <anurag.rana@naukri.com>
2020-09-10 16:10:59 +0530

    added program to convert linux file permission from octal to string representation

---
+++ permission.py
@@ -0,0 +1,23 @@
+# 
+# programm to convert from octal to string representation of linux file perminssions
+# Author: https://www.pythoncircle.com
+#
+
+def octal_to_string(octal):
+    result = ""
+    value_letters = [(4,"r"),(2,"w"),(1,"x")]
+    # Iterate over each of the digits in octal
+    for digit in [int(n) for n in str(octal)]:
+        # Check for each of the permissions values
+        for value, letter in value_letters:
+            if digit >= value:
+                result += letter
+                digit -= value
+            else:
+                result += '-'
+    return result
+    
+print(octal_to_string(755)) # Should be rwxr-xr-x
+print(octal_to_string(644)) # Should be rw-r--r--
+print(octal_to_string(750)) # Should be rwxr-x---
+print(octal_to_string(600)) # Should be rw-------
